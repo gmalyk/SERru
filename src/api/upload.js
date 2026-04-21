d95ea431ed0fdc53e573326425b612ba63d96b05
SpiderMan6699 <gregmalyk@gmail.com>
2026-04-21 21:09:52 +0300

    fix uploads docss

--- src/api/upload.js
+++ src/api/upload.js
@@ -2,7 +2,7 @@ import { post } from './client.js';
 
 export async function uploadFile(file, folder = 'images') {
   const formData = new FormData();
-  formData.append('file', file);
   formData.append('folder', folder);
+  formData.append('file', file);
   return post('/api/admin/upload', formData);
 }
